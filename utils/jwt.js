93c65864cc254ae601db5dc8713e09711b26aa39
Majoros Jozsef Mark <mmark0416@gmail.com>
2023-12-07 18:10:51 +0100

    change token time

--- utils/jwt.js
+++ utils/jwt.js
@@ -15,20 +15,20 @@ export const attachCookiesToResponse = ({ res, user, refreshToken }) => {
   const oneDay = 1000 * 60 * 60 * 24;
   const oneMonth = 1000 * 60 * 60 * 24 * 30;
 
-  const tenSec = 1000 * 5;
-  const oneMin = 1000 * 10;
+  const tenSec = 1000 * 10;
+  const oneMin = 1000 * 60;
 
   res.cookie("accessToken", accessTokenJWT, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     signed: true,
-    expires: new Date(Date.now() + tenSec),
+    expires: new Date(Date.now() + oneDay),
   });
 
   res.cookie("refreshToken", refreshTokenJWT, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     signed: true,
-    expires: new Date(Date.now() + oneMin),
+    expires: new Date(Date.now() + oneMonth),
   });
 };
